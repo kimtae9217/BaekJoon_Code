ae23240985e6934146d7e8e55284785cc7bc9adb
Kim TaeWon <kimtae9217@gmail.com>
2021-03-24 13:20:09 +0900

    Search_Maze.py

--- [백준 2178] 미로 탐색/Search_Maze.py
+++ [백준 2178] 미로 탐색/Search_Maze.py
@@ -26,4 +26,4 @@ while queue:
         if 0 <= nx < N and 0 <= ny < M:
             if visited[nx][ny] == 0 and matrix[nx][ny] == '1':
                 visited[nx][ny] = visited[x][y] + 1
-                queue.append((nx,ny))
+                queue.append((nx,ny)) 
