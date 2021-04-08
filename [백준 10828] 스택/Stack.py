7640c40e3b1efc9205d5d0229c42a98ea142010a
taewon <kimtae9217@gmail.com>
2021-04-08 23:22:18 +0900

    [Comment] 코드 수정 및 주석 추가

--- [백준 10828] 스택/Stack.py
+++ [백준 10828] 스택/Stack.py
@@ -1,25 +1,40 @@
 import sys
 
+# N의 정수 입력
 N = int(sys.stdin.readline())
+# stack 리스트 초기화
 stack = []
 
-def push(x): 
+
+# 정수 X를 스택에 넣는 연산
+def push(x):
     stack.append(x)
 
+
+# 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력
+# 만약 스택에 들어있는 정수가 없는 경우, -1 출력
 def pop():
-    if(not stack):
+    if not stack:
         return -1
     else:
         return stack.pop()
 
+
+# 스택에 들어있는 정수의 개수를 출력
 def size():
     return len(stack)
 
+
+# 스택이 비어있으면 1, 아니면 0 출력
 def empty():
     return 0 if stack else 1
 
+
+# 스택 가장 위에 있는 정수 출력
+# 만약 스택에 들어있는 정수가 없는 경우, -1 출력
 def top():
     return stack[-1] if stack else -1
+
 
 for _ in range(N):
     cmd = sys.stdin.readline().split()
@@ -34,4 +49,3 @@ for _ in range(N):
         print(empty())
     elif cmd[0] == "top":
         print(top())
-
